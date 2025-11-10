b3e3c32d74408ef69af67e7a7dc2d76a1afda4d2
ABOUCHELAILA <TON_EMAIL@exemple.com>
2025-11-10 12:27:45 +0100

    Initial commit

---
+++ src/main/java/com/sdia/ecomsdia/EcomSdiaApplication.java
@@ -0,0 +1,13 @@
+package com.sdia.ecomsdia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomSdiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomSdiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sdia/ecomsdia/EcomSdiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdia.ecomsdia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomSdiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
